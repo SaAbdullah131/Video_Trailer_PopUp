a4c3051d9f6300c0f04bf47e755f5cf861c78a11
SaAbdullah131 <cse.asabdullah@gmail.com>
2022-12-31 18:50:10 +0600

    javascript and video trailer popUp done

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+
 const btnElement=document.querySelector(".btn");
 const closeIconElement=document.querySelector(".close-icon");
 const trailerContainerElement=document.querySelector(".trailer-container");
 
+const videoElement=document.querySelector("video");
+
+// adding lister in button
 btnElement.addEventListener("click",()=>{
     trailerContainerElement.classList.remove("active");
+
 });
 
 closeIconElement.addEventListener("click",()=>{
-
+    trailerContainerElement.classList.add("active");
+    videoElement.pause();
+    videoElement.currentTime=0;
 })
